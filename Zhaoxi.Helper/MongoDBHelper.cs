4e1b0f4c688780c1093493c3ad3346fc1af17e85
zhangyu0308 <510181251@qq.com>
2020-05-26 22:15:40 +0800

    1
    添加帮助类

---
+++ Zhaoxi.Helper/MongoDBHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Zhaoxi.Helper
+{
+    public class MongoDBHelper
+    {
+
+    }
+}
